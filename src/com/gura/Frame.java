2bb62257488e4ec95c90083d0db1ffa64b2fba12
Gura-Andriy <gura3a@gmail.com>
2020-11-18 15:38:49 +0200

    Initial commit

---
+++ src/com/gura/Frame.java
@@ -0,0 +1,19 @@
+package com.gura;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+    public Frame() {
+        initUI();
+    }
+
+    private void initUI() {
+        var frame = new Noise();
+        //add(frame);
+
+        setSize(350, 250);
+        setTitle("Points");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
