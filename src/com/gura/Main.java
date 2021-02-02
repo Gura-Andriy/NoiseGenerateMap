78017dd8c9e15e00e6fd5ce94107e17040e4e43d
gura3 <gura3a@gmail.com>
2021-02-02 13:16:52 +0200

    now island in a center

--- src/com/gura/Main.java
+++ src/com/gura/Main.java
@@ -12,11 +12,6 @@ public class Main {
     private static final double FEATURE_SIZE = 24;
 
     public static void main(String[] args) throws IOException {
-//        EventQueue.invokeLater(() -> {
-//            var ex = new Frame();
-//            ex.setVisible(true);
-//        });
-
 
         PerlinNoise noise = new PerlinNoise((int) (Math.random() * 1000000000));
         BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -24,10 +19,11 @@ public class Main {
         {
             for (int x = 0; x < WIDTH; x++)
             {
-                double nx = x / FEATURE_SIZE - 10, ny = y / FEATURE_SIZE - 10;
+                double nx = x / FEATURE_SIZE - (FEATURE_SIZE * 0.43),
+                        ny = y / FEATURE_SIZE - (FEATURE_SIZE * 0.43);
 
                 double d = sqrt(nx*nx + ny*ny) / sqrt(50);
-                d = pow(d, 1.5);
+                d = pow(d, 2);
 
                 double e = noise.noise( nx, ny)
                         + 0.5 * noise.noise(2 * nx, 2 * ny)
@@ -36,7 +32,7 @@ public class Main {
                 double value = pow(e, 2);
 
                 int rgb = 0x0000FF * (int)((value + 1) * 127.5);
-                if (e < 0.1) rgb = 0x90CCFB;
+                if (e < 0.15) rgb = 0x90CCFB;
 
                 image.setRGB(x, y, rgb);
             }
